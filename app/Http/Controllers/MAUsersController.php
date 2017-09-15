aebd0ce951137d50457e508f9fbd0aa0e101fa6a
Arturas33 <jasinskis330@gmail.com>
2017-09-15 14:32:16 +0300

    added roles for user

--- app/Http/Controllers/MAUsersController.php
+++ app/Http/Controllers/MAUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MARoles;
 use App\Models\MAUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -75,8 +76,9 @@ class MAUsersController extends Controller
     public function show($id)
     {
         $user = MAUsers::find($id);
+        $roles = MARoles::all();
         if ($user->save()) {
-            return response()->json(['user' => $user], 200);
+            return response()->json(['user' => $user , 'roles' => $roles], 200);
         } else {
             return response()->json(['error' => 'User not found!'], 400);
         }
